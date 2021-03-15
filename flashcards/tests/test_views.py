ea057e79ab9dc3fbe6caa758f7f47f77f5b89c71
Agnieszka Platek <aga@platki.waw.pl>
2021-03-15 12:05:24 +0100

    Add unit tests for views

--- flashcards/tests/test_views.py
+++ flashcards/tests/test_views.py
@@ -1,77 +1,73 @@
-from rest_framework.test import force_authenticate
-from rest_framework.test import APIRequestFactory
 from django.test import TestCase
-from unittest import skip
-from rest_framework.test import APIClient
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import force_authenticate
 
+from users.models import User
 from ..models import FlashcardSet, Flashcard
 from ..views import FlashcardViewSet, FlashcardSetViewSet, FlashcardListView
-from users.models import User
 
 
 class FlashcardSetTest(TestCase):
 
     def setUp(self):
-        # self.client = APIClient()
         self.factory = APIRequestFactory()
         self.user = User.objects.create(username='Tester1')
+        self.endpoint = '/flashcard-sets/'
 
-    def test_get(self):
-        endpoint = '/flashcard-sets/'
-        request = self.factory.get(endpoint)
+    def test_get_flashcard_sets(self):
+        request = self.factory.get(self.endpoint)
         force_authenticate(request, user=self.user)
         response = FlashcardSetViewSet.as_view({'get': 'list'})(request)
         self.assertEqual(200, response.status_code)
 
-    def test_post(self):
-        endpoint = '/flashcard-sets/'
+    def test_create_new_flashcards_set(self):
         data = {'name': 'testing'}
-        request = self.factory.post(endpoint, data)
+        request = self.factory.post(self.endpoint, data)
         force_authenticate(request, user=self.user)
         response = FlashcardSetViewSet.as_view({'post': 'create'})(request)
         self.assertEqual(201, response.status_code)
 
-#
-#     @skip
-#     def test_get_all_users_flashcard_sets(self):
-#         pass
-#
-#     @skip
-#     def test_rename_flashcard_set(self):
-#         pass
-#
-#     @skip
-#     def test_delete_flashcard_set(self):
-#         pass
 
-# class FlashcardTest(TestCase):
-#
-#     def setUp(self):
-#         pass
-#
-#     def test_create_flashcard_with_all_fields_given(self):
-#         pass
-#
-#     def test_fail_to_create_if_missing_flashcard_set(self):
-#         pass
-#
-#     def test_fail_to_create_if_missing_front_or_back(self):
-#         pass
-#
-#     def test_update_flashcard_with_flashcard_set_and_one_field_given(self):
-#         pass
-#
-#     def test_fail_to_update_with_no_flashcard_set_given(self):
-#         pass
-#
-#     def test_fail_to_update_with_flashcard_set_given_but_no_front_and_no_back(self):
-#         pass
+class FlashcardTest(TestCase):
 
-# def test_to_get_all_users_flashcards(self):
-#     pass
-#
-# def test_to_get_all_flashcards_from_one_flashcard_set(self):
-#     pass
-#
-# def test_delete_flashcard(self):
-#     pass
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create(username='Tester1')
+        self.set1 = FlashcardSet.objects.create(name='set1', owner=self.user)
+        self.endpoint = '/flashcards/'
+
+    def test_get_flashcards(self):
+        request = self.factory.get(self.endpoint)
+        force_authenticate(request, user=self.user)
+        response = FlashcardViewSet.as_view({'get': 'list'})(request)
+        self.assertEqual(200, response.status_code)
+
+    def test_create_flashcard(self):
+        data = {'flashcard_set': self.set1.pk, 'front': 'question', 'back': 'answer'}
+        request = self.factory.post(self.endpoint, data)
+        force_authenticate(request, user=self.user)
+        response = FlashcardViewSet.as_view({'post': 'create'})(request)
+        self.assertEqual(201, response.status_code)
+
+
+class FlashcardListTest(TestCase):
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create(username='Tester1')
+        set1 = FlashcardSet.objects.create(name='set1', owner=self.user)
+        self.set1_pk = set1.pk
+        self.endpoint = '/flashcard-list/' + str(self.set1_pk)
+
+    def test_get_flashcards(self):
+        request = self.factory.get(self.endpoint)
+        force_authenticate(request, user=self.user)
+        response = FlashcardListView.as_view()(request)
+        self.assertEqual(200, response.status_code)
+
+    def test_create_flashcard_not_allowed(self):
+        data = {'flashcard_set': self.set1_pk, 'front': 'question', 'back': 'answer'}
+        request = self.factory.post(self.endpoint, data)
+        force_authenticate(request, user=self.user)
+        response = FlashcardListView.as_view()(request)
+        self.assertEqual(405, response.status_code)
