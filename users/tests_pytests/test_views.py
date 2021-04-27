f5d3e566f98b23ed5f50804855839fdfeff07047
Agnieszka Platek <aga@platki.waw.pl>
2021-04-27 10:26:04 +0200

    Add demo_token fixture

--- users/tests_pytests/test_views.py
+++ users/tests_pytests/test_views.py
@@ -9,47 +9,40 @@ User = get_user_model()
 
 
 @pytest.fixture
-def demo_url():
-    return reverse('demo')
+def demo_token(client):
+    response = client.put(reverse('demo'))
+    demo_token = Token(response.data['token'])
+    yield demo_token
+    demo_token.delete()
 
 
 @pytest.mark.django_db
 class TestDemoUser:
 
-    def test_new_demo_users_token_created(self, client, demo_url):
-        response = client.put(demo_url)
-        demo_token = response.data['token']
-        assert response.status_code == status.HTTP_201_CREATED
-        assert demo_token == str(Token.objects.last())
-
-    def test_new_demo_user_created(self, client, demo_url):
-        response = client.put(demo_url)
+    def test_new_demo_users_token_created(self, client):
+        response = client.put(reverse('demo'))
         demo_token = Token(response.data['token'])
+        assert response.status_code == status.HTTP_201_CREATED
+        assert demo_token == Token.objects.last()
+
+    def test_new_demo_user_created(self, demo_token):
         assert demo_token == User.objects.last().auth_token
 
-    def test_new_demo_user_has_one_example_set(self, client, demo_url):
-        response = client.put(demo_url)
-        demo_token = Token(response.data['token'])
+    def test_new_demo_user_has_one_example_set(self, demo_token):
         user = User.objects.get(auth_token=demo_token)
         assert user.num_sets == 1
 
-    def test_new_demo_user_has_five_example_flashcards(self, client, demo_url):
-        response = client.put(demo_url)
-        demo_token = Token(response.data['token'])
+    def test_new_demo_user_has_five_example_flashcards(self, demo_token):
         user = User.objects.get(auth_token=demo_token)
         assert user.num_flashcards == 5
 
-    def test_demo_user_retrieved_from_cookie(self, client, demo_url):
-        response = client.put(demo_url)
-        demo_token = Token(response.data['token'])
-        response2 = client.put(demo_url)
+    def test_demo_user_retrieved_from_cookie(self, client, demo_token):
+        response2 = client.put(reverse('demo'))
         demo_token2 = Token(response2.data['token'])
         assert demo_token == demo_token2
 
-    def test_two_different_demo_users_when_no_cookie(self, client, demo_url):
-        response = client.put(demo_url)
-        demo_token = Token(response.data['token'])
+    def test_two_different_demo_users_when_no_cookie(self, demo_token):
         client2 = APIClient()
-        response2 = client2.put(demo_url)
+        response2 = client2.put(reverse('demo'))
         demo_token2 = Token(response2.data['token'])
         assert demo_token != demo_token2
